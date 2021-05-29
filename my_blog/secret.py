cdd450a27f4d9644e4614dbdeedd0e7e96e06476
Daniel Zhang <zhangster12@gmail.com>
2021-05-29 01:09:41 -0400

    make it better

---
+++ my_blog/secret.py
@@ -0,0 +1,6 @@
+secret_key = 'django-insecure-#(z2i-s$3&zd_o37ch+ci8t$rdlg^vhvr*x-6=lwo-@bwo65g6'
+allowed_host = 'daniel-zhang-blog.herokuapp.com'
+name = 'd4029kf2q8jvuj'
+user = 'jfchkpalaiszfp'
+password = '5342bbdcd000e2c107092100d28b7d25da61e6585a84770afea918ce98f42f74'
+host = 'ec2-52-86-25-51.compute-1.amazonaws.com'
